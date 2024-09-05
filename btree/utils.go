da1e6576d1ba3896206153ec3da89c008834defb
Arush Gupta <g.arush@iitg.ac.in>
2024-09-05 19:11:43 +0530

    assertion statement added in nodesplit3 function

--- btree/utils.go
+++ btree/utils.go
@@ -176,24 +176,24 @@ func nodeSplit2(left BNode, right BNode, old BNode) {
 }
 
 // split a node if it's too big. the results are 1~3 nodes.
-func nodeSplit3(old BNode) (uint16, [3]BNode) {
+func nodeSplit3(old BNode) (uint16, []BNode) {
 	if old.nbytes() <= BTREE_PAGE_SIZE {
 		old.data = old.data[:BTREE_PAGE_SIZE]
-		return 1, [3]BNode{old}
+		return 1, []BNode{old}
 	}
 	left := BNode{make([]byte, 2*BTREE_PAGE_SIZE)} // might be split later
 	right := BNode{make([]byte, BTREE_PAGE_SIZE)}
 	nodeSplit2(left, right, old)
 	if left.nbytes() <= BTREE_PAGE_SIZE {
 		left.data = left.data[:BTREE_PAGE_SIZE]
-		return 2, [3]BNode{left, right}
+		return 2, []BNode{left, right}
 	}
 	// the left node is still too large
 	leftleft := BNode{make([]byte, BTREE_PAGE_SIZE)}
 	middle := BNode{make([]byte, BTREE_PAGE_SIZE)}
 	nodeSplit2(leftleft, middle, left)
-	utils.Assert(leftleft.nbytes() <= BTREE_PAGE_SIZE, "")
-	return 3, [3]BNode{leftleft, middle, right}
+	utils.Assert(leftleft.nbytes() <= BTREE_PAGE_SIZE, "Requires more than 3 splits")
+	return 3, []BNode{leftleft, middle, right}
 }
 
 // replace a link with multiple links
